80ee043910551d6cfe35ce150151d0a710d73b7d
Carlos Crespo <carlos.crespog@gmail.com>
2013-09-09 13:21:10 +0200

    merging project GCalendar into subdirectory

---
+++ GCalendar/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	consumer_key		: 'YOUR CONSUMER_KEY',
+	consumer_secret : 'YOUR_CONSUMER_SECRET',
+	redirect_url : 'YOUR REDIRECT_URL',
+	access_token : 'YOUR_ACCESS_TOKEN',
+	refresh_token : 'YOUR_REFRESH_TOKEN',
+	calendarId:'YOUR_CALENDAR_ID'
+}
